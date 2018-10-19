368c05ba3fe75a1330a819d0680e9dfb333aa8e9
Mike S <mstange@dthera.com>
2018-10-19 12:24:44 -0700

    added navbar

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div id="navbar">
+        <img
+          className="navbar-logo"
+          src={require('../assets/images/reminx-logo.png')}
+          alt="ReminX Logo"
+          width={250}
+        />
+        <div className="navbar-container-right">
+          <h3 className="rx-header-title">Data Pipeline Dashboard</h3>
+          <img
+            className="sidebar-avatar"
+            alt="Bar Graph"
+            src={require('../assets/images/bar-graph.png')}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
