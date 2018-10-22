c99fe03dd7e3bae153ef81efa312dd91f998a8a3
Mike S <mstange@dthera.com>
2018-10-22 12:47:31 -0700

    added search to WatchData

--- client/src/containers/WatchData.js
+++ client/src/containers/WatchData.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
 import moment from 'moment';
 import API from "../utils/API";
 import muze, { DataModel } from 'muze';
 import DateRangePicker from '../components/DateRangePicker';
+import Notification from '../components/Notification';
+import WatcherSearch from '../components/WatcherSearch';
 
 const env = muze();
 
@@ -13,16 +14,47 @@ class WatchData extends Component {
     super(props);
     this.state = {
       watchData: [],
+      activeWatcherAccounts: [],
       activeWatcherIds: [],
+      activeFamilyCodes: [],
       displayError: false,
+      selectedOption: 'minutesWatched',
+      currentWatcherId: 0,
+      currentFamilyCode: '',
+      renderMode: 'familyCode',
+      hasAddedFamilyCodes: false,
     };
   }
 
   componentDidMount() {
+    this.getWatchData();
+  }
+
+  componentDidUpdate() {
+    if (this.state.activeWatcherAccounts.length && this.state.watchData.length && !this.state.hasAddedFamilyCodes) {
+      this.addFamilyCodesToWatchData();
+    }
+  }
+
+  addFamilyCodesToWatchData = () => {
+    const { activeWatcherAccounts } = this.state;
+    const watchData = this.state.watchData.slice();
+    watchData.forEach(d => {
+      const idx = activeWatcherAccounts.findIndex(account => account.patient_account_id === d.watcher_id);
+      if (idx !== -1) {
+        d['Family Code'] = activeWatcherAccounts[idx].read_write_share_code;
+      }
+    });
+    // console.log('watch data:', watchData);
+    this.setState({ watchData, hasAddedFamilyCodes: true });
+  }
+
+  getWatchData = () => {
     document.getElementById('chart-container').innerHTML = '';
     this.setState({ watchData: [] });
     API.getWatchData()
       .then(res => {
+        this.props.setDisplayString('Minutes Watched by Day (Active Accounts)');
         this.setState({
           watchData: res.data
             .slice()
@@ -30,24 +62,19 @@ class WatchData extends Component {
             .map(d => {
               d['Date'] = moment.utc(d.date).format('M/DD/YY');
               d['Total Watch Minutes'] = (d.sum_watch / 1000 / 60).toFixed();
+              d['Watcher ID'] = d.watcher_id;
               return d;
             }),
-          displayString: 'Minutes Watched by Day (Active Accounts)',
-        }, () => {
-          console.log('cleaned watchData:', this.state.watchData);
-          this.props.setDisplayString('Minutes Watched by Day (Active Accounts)');
         });
       })
       .catch(err => console.log(err.message));
-
     API.getActiveWatcherAccounts()
       .then(res => {
         // console.log('active customer accounts:', res.data);
         this.setState({
-          customerAccounts: res.data,
+          activeWatcherAccounts: res.data,
           activeWatcherIds: res.data.reduce((acc, d) => [...acc, d.patient_account_id], []),
-        }, () => {
-          // console.log('active watcher IDs:', this.state.activeWatcherIds);
+          activeFamilyCodes: res.data.reduce((acc, d) => [...acc, d.read_write_share_code], []),
         });
       })
       .catch(err => console.log(err.message));
@@ -58,9 +85,27 @@ class WatchData extends Component {
   }
 
   renderWatchData = (range = null) => {
-    const { watchData } = this.state;
+    const { watchData, renderMode } = this.state;
     if (watchData.length < 1) return null;
-    let filteredWatchData = watchData.filter(d => this.state.activeWatcherIds.includes(d.watcher_id));
+    let filteredWatchData = [];
+
+    // filter data dependent on renderMode
+    if (renderMode === 'watcherId') {
+      filteredWatchData = watchData.filter(d => this.state.activeWatcherIds.includes(d['Watcher ID']));
+    } else {
+      filteredWatchData = watchData.filter(d => this.state.activeFamilyCodes.includes(d['Family Code']));
+    }
+    // console.log('filtered watch data:', filteredWatchData);
+    if (filteredWatchData.length < 1) return null;
+    // check for watcher ID filter
+    if (renderMode === 'watcherId' && this.state.currentWatcherId !== 0) {
+      filteredWatchData = filteredWatchData.filter(d => d['Watcher ID'] === this.state.currentWatcherId);
+    }
+    // check for family code filter
+    if (renderMode === 'familyCode' && this.state.currentFamilyCode !== '') {
+      filteredWatchData = filteredWatchData.filter(d => d['Family Code'] === this.state.currentFamilyCode);
+    }
+
     if (range) {
       const startDate = range.startDate.format('M/DD/YY');
       const endDate = range.endDate.format('M/DD/YY');
@@ -80,8 +125,6 @@ class WatchData extends Component {
       const node = { name: key, type: 'dimension' };
       if (key === 'Total Watch Minutes') {
         node.type = 'measure';
-      // } else if (key === 'date') {
-      //   node.subtype = 'temporal';
       }
       schema.push(node);
     });
@@ -93,7 +136,7 @@ class WatchData extends Component {
       .height(480)
       .rows(['Total Watch Minutes'])
       .columns(['Date'])
-      .color('watcher_id')
+      .color(renderMode === 'watcherId' ? 'Watcher ID' : 'Family Code')
       .mount('#chart-container')
     ;
   }
@@ -103,20 +146,53 @@ class WatchData extends Component {
     this.setState({ displayError: false });
   }
 
+  handleWatcherSelected = (watcher, renderMode) => {
+    if (renderMode === 'watcherId') {
+      this.setState({ currentWatcherId: watcher, renderMode });
+    } else {
+      this.setState({ currentFamilyCode: watcher, renderMode });
+    }
+  }
+
+  handleInputChange = (e) => {
+    e.preventDefault();
+    const { name } = e.target;
+    this.setState({ selectedOption: name });
+  }
+
   renderDashboard = () => {
     const { watchData } = this.state;
     if (this.state.watchData.length < 1) return null;
     const activeUserWatchData = watchData
       .slice()
-      .filter(d => this.state.activeWatcherIds.includes(d.watcher_id))
-    console.log('data:', activeUserWatchData);
+      .filter(d => this.state.activeWatcherIds.includes(d.watcher_id));
     if (activeUserWatchData.length < 1) return null;
     return (
-      <DateRangePicker
-        onDateRangePicked={(range) => this.renderWatchData(range)}
-        minDate={moment(activeUserWatchData[0].Date, 'M/DD/YY')}
-        maxDate={moment(activeUserWatchData[activeUserWatchData.length - 1].Date, 'M/DD/YY')}
-      />
+      <div className="data-dashboard">
+        <div className="form-input-container">
+          <label className="checkbox-label">
+            <input
+              name="minutesWatched"
+              type="checkbox"
+              checked={this.state.selectedOption === 'minutesWatched'}
+              onChange={this.handleInputChange}
+            />
+            {'Minutes Watched'}
+          </label>
+          {this.state.hasAddedFamilyCodes && (
+            <WatcherSearch
+              activeUserData={activeUserWatchData}
+              activeWatcherAccounts={this.state.activeWatcherAccounts}
+              onWatcherIdSelected={this.handleWatcherSelected}
+            />
+          )}
+        </div>
+        <DateRangePicker
+          onDateRangePicked={(range) => this.renderWatchData(range)}
+          minDate={moment(activeUserWatchData[0].Date, 'M/DD/YY')}
+          maxDate={moment(activeUserWatchData[activeUserWatchData.length - 1].Date, 'M/DD/YY')}
+        />
+      </div>
     );
   }
 
@@ -125,23 +201,10 @@ class WatchData extends Component {
       return null;
     }
     return (
-      <div className="notification-container">
-        <h2>Error!</h2>
-        { this.state.errorMessage }
-        <br />
-        <br />
-        <form>
-          <Button
-            bsStyle="default"
-            bsSize="large"
-            onClick={this.handleCloseNotification}
-            type="submit"
-          >
-            OK
-          </Button>
-        </form>
-
-      </div>
+      <Notification
+        errorMessage={this.state.errorMessage}
+        onCloseNotification={() => this.setState({ displayError: false })}
+      />
     )
   }
 
