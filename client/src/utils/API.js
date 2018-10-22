467617e78daf3b736e6cc2841a6ed658dc122a39
Mike S <mstange@dthera.com>
2018-10-22 09:45:04 -0700

    added getActiveSmsAccounts

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export default {
   searchBing: (topic, offset) => axios.get(`/images/${topic}/${offset}`),
   getWatchData: () => axios.get('/watch'),
-  getActiveAccounts: () => axios.get('/accounts/active'),
+  getActiveWatcherAccounts: () => axios.get('/accounts/watchers'),
+  getActiveSmsAccounts: () => axios.get('accounts/sms'),
   getMediaUploadsData: () => axios.get('/media/uploads'),
   getEmotionData: () => axios.get('/emotion'),
   getSmsData: () => axios.get('/sms'),
