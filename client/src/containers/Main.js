582bfb54f52c5c261ae9a447dca07a95f9185d87
Mike S <mstange@dthera.com>
2018-10-26 16:18:30 -0700

    get account info in Main

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import DataDisplay from './DataDisplay';
+import API from '../utils/API';
+import { setAllAccounts, setActiveAccounts } from '../redux/actions';
 import '../css/style.css';
 import 'muze/dist/muze.css';
 
@@ -12,6 +14,25 @@ class Main extends Component {
     this.state = {
       displayMode: '',
     };
+  }
+
+  componentDidMount() {
+    this.getAccounts();
+  }
+
+  getAccounts = () => {
+    API.getAllAccounts()
+      .then(res => {
+        console.log('all accounts res:', res.data);
+        this.props.setAllAccounts(res.data);
+      })
+      .catch(err => console.log(err.message));
+    API.getActiveWatcherAccounts()
+      .then(res => {
+        console.log('active customer accounts res:', res.data);
+        this.props.setActiveAccounts(res.data);
+      })
+      .catch(err => console.log(err.message));
   }
 
   render() {
@@ -33,6 +54,8 @@ const mapStateToProps = () => ({
 });
 
 const mapDispatchToProps = {
+  setAllAccounts,
+  setActiveAccounts,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Main);
