9df8a9e0d2849c195af2217f9e10fb7d36163e65
Mike S <mstange@dthera.com>
2018-10-29 16:59:13 -0700

    move height and width to spinner

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ClipLoader } from 'react-spinners';
 
+const CHART_CONTAINER_HEIGHT = window.innerHeight - 580;
+const CHART_CONTAINER_WIDTH = window.innerWidth - 280;
+
 const Spinner = (props) => {
   return (
-    <div style={{ display: 'flex', paddingTop: 100, justifyContent: 'center', height: props.height, width: props.width }}>
+    <div style={{ display: 'flex', paddingTop: 100, justifyContent: 'center', height: CHART_CONTAINER_HEIGHT, width: CHART_CONTAINER_WIDTH }}>
       <ClipLoader size={36} color="#57337e" />
     </div>
   );
