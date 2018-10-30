99b8c0c889c98735a2c6b51c8d1ab803e1e34a68
Mike S <mstange@dthera.com>
2018-10-30 13:33:48 -0700

    add loading prop to Spinner

--- client/src/components/Spinner.js
+++ client/src/components/Spinner.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ClipLoader } from 'react-spinners';
 
-const CHART_CONTAINER_HEIGHT = window.innerHeight - 580;
-const CHART_CONTAINER_WIDTH = window.innerWidth - 280;
+const CHART_CONTAINER_HEIGHT = window.innerHeight - 760;
+const CHART_CONTAINER_WIDTH = window.innerWidth - 310;
 
-const Spinner = (props) => {
+const Spinner = props => {
+  if (!props.loading) return null;
   return (
     <div style={{ display: 'flex', paddingTop: 100, justifyContent: 'center', height: CHART_CONTAINER_HEIGHT, width: CHART_CONTAINER_WIDTH }}>
       <ClipLoader size={36} color="#57337e" />
     </div>
   );
-}
+};
+
+Spinner.propTypes = {
+  loading: PropTypes.bool.isRequired,
+};
 
 export default Spinner;
