0ccac69cfa08b2f128a8252bcc323eae23e0c50a
Mike S <mstange@dthera.com>
2018-10-29 17:00:33 -0700

    move device data chart to Chart

--- client/src/containers/DeviceData.js
+++ client/src/containers/DeviceData.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import API from "../utils/API";
-import muze, { DataModel } from 'muze';
 import moment from 'moment';
 import DataDashboard from '../components/DataDashboard';
 import Notification from '../components/Notification';
 import Spinner from '../components/Spinner';
-import { setSearchValue } from '../redux/actions';
-
-const env = muze();
-const CHART_CONTAINER_HEIGHT = window.innerHeight - 580;
-const CHART_CONTAINER_WIDTH = window.innerWidth - 280;
+import KpiData from '../components/KpiData';
+import Chart from '../components/Chart';
+import {
+  setCurrentFamilyCode,
+  setCurrentDeviceId,
+} from '../redux/actions';
 
 class DeviceData extends Component {
   constructor(props) {
@@ -23,11 +23,10 @@ class DeviceData extends Component {
       activeFamilyCodes: [],
       displayError: false,
       errorMessage: '',
-      currentDeviceId: 0,
-      currentFamilyCode: '',
-      renderMode: 'familyCode',
+      displayMode: 'familyCode',
       checked: false,
       loadingData: false,
+      range: null,
     };
   }
 
@@ -50,6 +49,7 @@ class DeviceData extends Component {
               d['Device ID'] = d.device_id;
               d['Family Code'] = d.code;
               d['Device Pings'] = 1;
+              d.create_date = d.date;
               return d;
             }),
             loadingData: false,
@@ -77,85 +77,21 @@ class DeviceData extends Component {
     }
   }
 
-  renderDeviceData = (range = null) => {
-    const { deviceData, renderMode } = this.state;
-    if (deviceData.length < 1 || this.state.activeFamilyCodes.length < 1) return null;
-    let filteredDeviceData = deviceData.slice();
-
-    if (renderMode === 'familyCode' && this.state.checked) {
-      filteredDeviceData = deviceData.filter(d => this.state.activeFamilyCodes.includes(d['Family Code']));
-    }
-
-    // check for account ID filter
-    if (renderMode === 'deviceId' && this.state.currentDeviceId !== 0) {
-      filteredDeviceData = filteredDeviceData.filter(d => d['Device ID'] === this.state.currentDeviceId);
-    }
-
-    if (renderMode === 'familyCode' && this.state.currentFamilyCode !== '') {
-      filteredDeviceData = filteredDeviceData.filter(d => d['Family Code'] === this.state.currentFamilyCode);
-    }
-
-    // check for date range filter
-    if (range) {
-      const startDate = range.startDate.format('M/DD/YY');
-      const endDate = range.endDate.format('M/DD/YY');
-      filteredDeviceData = filteredDeviceData.filter(d => {
-        if (moment(d.Date, 'M/DD/YY').diff(moment(startDate, 'M/DD/YY'), 'days') >= 0 && moment(endDate, 'M/DD/YY').diff(moment(d.Date, 'M/DD/YY'), 'days') >= 0) {
-          return true;
-        }
-        return false;
-      });
-    }
-
-    // throw error if no records remain after filter
-    if (filteredDeviceData.length < 1) {
-      this.triggerError('There are no data events over the selected date range');
-      return null;
-    }
-
-    const schema = [];
-    Object.keys(filteredDeviceData[0]).forEach(key => {
-      const node = { name: key, type: 'dimension' };
-      if (key === 'Device Pings') {
-        node.type = 'measure';
-      }
-      schema.push(node);
-    });
-
-    const dm = new DataModel(filteredDeviceData, schema);
-    const canvas = env.canvas();
-    canvas
-      .data(dm)
-      .width(CHART_CONTAINER_WIDTH)
-      .height(CHART_CONTAINER_HEIGHT)
-      // .layers([{
-      //   mark: 'arc',
-      //     encoding: {
-      //       angle: 'Device Pings',
-      //     },
-      // }])
-      // .rows([])
-      // .columns([])
-      .rows(['Device Pings'])
-      .columns(['Date'])
-      .color(renderMode === 'familyCode' ? 'Family Code' : 'Device ID')
-      .mount('#chart-container')
-      // .size('batt')
-    ;
-    return null;
-  }
-
   handleInputChange = (e) => {
     e.preventDefault();
     const { name } = e.target;
     this.setState({ selectedOption: name });
   }
 
-  onDeviceSelected = (device, renderMode) => {
-    if (renderMode === 'familyCode') {
-      this.setState({ currentFamilyCode: device, currentDeviceId: 0, renderMode, checked: false });
+  onDeviceSelected = (device, displayMode) => {
+    if (displayMode === 'familyCode') {
+      this.setState({ displayMode, checked: false });
+      this.props.setCurrentFamilyCode(device);
+      this.props.setCurrentDeviceId(0);
     } else {
-      this.setState({ currentDeviceId: device, currentFamilyCode: '', renderMode, checked: false });
+      this.setState({ displayMode, checked: false });
+      this.props.setCurrentFamilyCode('');
+      this.props.setCurrentDeviceId(device);
     }
   }
 
@@ -173,18 +109,13 @@ class DeviceData extends Component {
             document.getElementById('chart-container').innerHTML = '';
             this.setState({
               checked: e.target.checked,
-              renderMode: e.target.checked ? 'familyCode' : 'deviceId',
-              currentFamilyCode: '',
-              currentDeviceId: 0,
-             });
-             this.props.setSearchValue('');
+              displayMode: e.target.checked ? 'familyCode' : 'deviceId',
+            });
           },
         }]}
         searchType="device"
         onSearchTargetSelected={this.onDeviceSelected}
-        onDateRangePicked={range => this.renderDeviceData(range)}
-        clearFilterButtonDisabled={this.state.currentFamilyCode === '' && this.state.currentDeviceId === 0}
-        clearFilterButtonOnClick={() => this.setState({ currentFamilyCode: '', currentDeviceId: 0 })}
+        onDateRangePicked={range => this.setState({ range })}
       />
     );
   }
@@ -192,7 +123,7 @@ class DeviceData extends Component {
   renderSpinner = () => {
     if (!this.state.loadingData) return null;
     return (
-      <Spinner height={CHART_CONTAINER_HEIGHT} width={CHART_CONTAINER_WIDTH} />
+      <Spinner />
     );
   }
 
@@ -208,12 +139,34 @@ class DeviceData extends Component {
     );
   }
 
+  renderChart = () => {
+    const { deviceData, displayMode, activeFamilyCodes, checked, range } = this.state;
+    if (deviceData.length < 1 || activeFamilyCodes.length < 1) return null;
+    return (
+      <Chart
+        data={deviceData}
+        chartType="device"
+        range={range}
+        displayMode={displayMode}
+        activeFamilyCodes={activeFamilyCodes}
+        active={checked}
+        triggerError={this.triggerError}
+        rows={'Device Pings'}
+        columns={'Date'}
+        color={this.state.displayMode === 'familyCode' ? 'Family Code' : 'Device ID'}
+      />
+    );
+  }
+
   render() {
     return (
       <div className="data-container">
+        <KpiData
+          kpiData={this.state.deviceData}
+        />
         <div id="chart-container">
           {this.renderSpinner()}
-          {this.renderDeviceData()}
+          {this.renderChart()}
         </div>
         {this.renderDashboard()}
         {this.renderNotification()}
@@ -226,11 +179,12 @@ DeviceData.propTypes = {
   setDisplayString: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = () => ({
+const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = {
-  setSearchValue,
+  setCurrentFamilyCode,
+  setCurrentDeviceId,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(DeviceData);
