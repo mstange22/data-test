5ba92c1f922a3b36545c3cda3775a8940f520336
Mike S <mstange@dthera.com>
2018-10-29 17:02:57 -0700

    add chart to SalesData

--- client/src/containers/SalesData.js
+++ client/src/containers/SalesData.js
@@ -1,52 +1,118 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import API from "../utils/API";
 import Spinner from '../components/Spinner';
 import KpiData from '../components/KpiData';
-
-const CHART_CONTAINER_HEIGHT = window.innerHeight - 580;
-const CHART_CONTAINER_WIDTH = window.innerWidth - 280;
+import Chart from '../components/Chart';
+import Notification from '../components/Notification';
 
 class SalesData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sales: [],
+      salesData: [],
+      totalSalesData: [],
       loadingData: false,
+      hasCalculatedDailySalesTotal: false,
     };
   }
 
   componentDidMount() {
-    // document.getElementById('chart-container').innerHTML = '';
     this.props.setDisplayString('Sales Data');
     this.setState({ loadingData: true });
     API.getSalesData()
       .then(res => {
-          console.log('sales:', res.data);
+          console.log('salesData:', res.data);
           this.setState({
-            // mediaUploads: res.data.filter(d => d.type === 'image' || d.type === 'video'),
-            sales: res.data,
+            salesData: res.data
+              .slice()
+              .sort((a, b) => a.create_date < b.create_date ? -1 : 1)
+              .map(d => {
+                d['Date'] = moment.utc(d.create_date).format('M/DD/YY');
+                d['Sales'] = 1;
+                return d;
+              }),
             loadingData: false,
           });
       })
       .catch(err => console.log(err.message));
   }
 
+  componentDidUpdate() {
+    if (this.state.salesData.length > 1 && !this.state.hasCalculatedDailySalesTotal) {
+      let totalSalesData = [];
+      let cumulativeSales = 0;
+      const dailySales = this.state.salesData
+        .reduce((accum, d) => {
+          if (accum[d['Date']]) accum[d['Date']]++;
+          else accum[d['Date']] = 1;
+          return accum;
+        }, {});
+      Object.keys(dailySales).forEach(key => {
+        totalSalesData.push({
+          Date: key,
+          Sales: dailySales[key] + cumulativeSales,
+        });
+        cumulativeSales += dailySales[key];
+      });
+      this.setState({ totalSalesData, hasCalculatedDailySalesTotal: true });
+    }
+  }
+
+  triggerError = (errorMessage) => {
+    document.getElementById('chart-container').innerHTML = '';
+    if (!this.state.displayError) {
+      this.setState({ displayError: true, errorMessage });
+    }
+  }
+
+  renderNotification = () => {
+    if (!this.state.displayError) {
+      return null;
+    }
+    return (
+      <Notification
+        errorMessage={this.state.errorMessage}
+        onCloseNotification={() => this.setState({ displayError: false })}
+      />
+    );
+  }
+
   renderSpinner = () => {
     if (!this.state.loadingData) return null;
     return (
-      <Spinner height={CHART_CONTAINER_HEIGHT} width={CHART_CONTAINER_WIDTH} />
+      <Spinner />
+    );
+  }
+
+  renderChart = () => {
+    const { totalSalesData } = this.state;
+    if (totalSalesData.length < 1) return null;
+    return (
+      <Chart
+        data={this.state.salesData}
+        chartType="sales"
+        displayMode={null}
+        rows={'Sales'}
+        columns={'Date'}
+        triggerError={this.triggerError}
+      />
     );
   }
 
   render() {
     return (
       <div className="data-container">
-          {this.renderSpinner()}
           <KpiData
-            kpiData={this.state.sales}
+            kpiData={this.state.salesData}
           />
+          <div id="chart-container">
+            {this.renderSpinner()}
+            {this.renderChart()}
+          </div>
+          {this.renderNotification()}
       </div>
     );
   }
