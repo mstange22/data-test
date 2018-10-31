81122b1621e964b2625560ad8eb8340266621bef
Mike S <mstange@dthera.com>
2018-10-30 18:36:58 -0700

    commented out icon import

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { connect } from 'react-redux';
 // import { toggleSidebar } from '../../modules/Album/albumActions';
 
